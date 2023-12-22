72c72d928613f5b054dd4cbb2b2fd10e4dc8c1b1
Tryxis <47526388+Tryxis@users.noreply.github.com>
2023-12-22 11:11:37 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Eye_Helper
+{
+    partial class EyeHelper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.TimerLabel = new System.Windows.Forms.Label();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SecondsTimer = new System.Windows.Forms.Timer(this.components);
+            this.RestTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(776, 104);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Eye Helper";
+            this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimerLabel.Location = new System.Drawing.Point(12, 153);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(776, 95);
+            this.TimerLabel.TabIndex = 1;
+            this.TimerLabel.Text = "20 Minutes 0 Seconds";
+            this.TimerLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(253, 276);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(140, 43);
+            this.ResetButton.TabIndex = 2;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(415, 276);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(140, 43);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(253, 325);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(302, 43);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // SecondsTimer
+            // 
+            this.SecondsTimer.Interval = 1000;
+            this.SecondsTimer.Tick += new System.EventHandler(this.SecondsTimer_Tick);
+            // 
+            // RestTimer
+            // 
+            this.RestTimer.Interval = 1000;
+            this.RestTimer.Tick += new System.EventHandler(this.RestTimer_Tick);
+            // 
+            // EyeHelper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.TitleLabel);
+            this.Name = "EyeHelper";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label TimerLabel;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Timer SecondsTimer;
+        private System.Windows.Forms.Timer RestTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eye_Helper
+{
+    public partial class EyeHelper : Form
+    {
+        public EyeHelper()
+        {
+            InitializeComponent();
+        }
+
+        int timeLeftSeconds = 0;
+        int timeLeftMinutes = 20;
+        int restSeconds = 21;
+        bool isResting = false;
+
+        private void SecondsTimer_Tick(object sender, EventArgs e)
+        {
+            // Decrement seconds
+            if(timeLeftSeconds > 0 && timeLeftMinutes > 0)
+            {
+                timeLeftSeconds--;
+                TimerLabel.Text = timeLeftMinutes + " Minutes " + timeLeftSeconds + " Seconds";
+            }
+            // Decrement minutes
+            else if(timeLeftMinutes > 0 && timeLeftSeconds == 0)
+            {
+                timeLeftMinutes--;
+                timeLeftSeconds = 60;
+                TimerLabel.Text = timeLeftMinutes + " Minutes " + timeLeftSeconds + " Seconds";  
+            }
+            // Remove minutes from the TimerLabel when equal to 0.
+            else if (timeLeftMinutes == 0 && timeLeftSeconds > 0)
+            {
+                timeLeftSeconds--;
+                TimerLabel.Text = timeLeftSeconds + " Seconds";
+            }
+            // Stop the timer
+            else
+            {
+  
+                SecondsTimer.Stop();
+                isResting = true;
+                TimerLabel.Text = "Time to rest! Click Start and focus your eyes on something over 20 meters away";
+            }
+        }
+
+        private void RestTimer_Tick(object sender, EventArgs e)
+        {
+            if(restSeconds > 0)
+            {
+                restSeconds--;
+                TimerLabel.Text = "Rest for " + restSeconds + " Seconds";
+            }
+            else
+            {
+                TimerLabel.Text = "Rest Completed";
+                RestTimer.Stop();
+                ResetValues();
+            }   
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if(isResting==false)
+            {
+                SecondsTimer.Start();
+            }
+            else if(isResting==true)
+            {
+                RestTimer.Start();
+            }
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            if (isResting == false)
+            {
+                SecondsTimer.Stop();
+            }
+            else if (isResting == true)
+            {
+                RestTimer.Stop();
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            SecondsTimer.Stop();
+            RestTimer.Stop();
+            timeLeftSeconds = 0;
+            timeLeftMinutes = 20;
+            TimerLabel.Text = timeLeftMinutes + " Minutes " + timeLeftSeconds + " Seconds";
+        }
+
+        private void ResetValues()
+        {
+            timeLeftSeconds = 0;
+            timeLeftMinutes = 20;
+            restSeconds = 21;
+            isResting = false;
+        }
+       
+    }
+
+
+}
